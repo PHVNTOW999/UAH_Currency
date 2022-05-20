d161937141774685ffdba0a8a9ed085e2ee83b55
Ali Tugaev <ali.tim03.at@gmail.com>
2022-05-20 22:55:30 +0600

    Push

---
+++ script.js
@@ -0,0 +1,30 @@
+function GET_CURR_API() {
+    let url = "./db.json"
+    fetch(url)
+        .then((res) => {
+            return res.json()
+        })
+        .then((data) => {
+            let main = document.querySelector("#main")
+
+            var currency = data.exchangeRate.map((curr) => {
+                return '<div class="currBlock">' +
+                '<div class="currBlock__id">' +
+                    '<p class="currBlock__id-baseCurr"> ' + curr.baseCurrency + '</p>' +
+                    '<p class="currBlock__id-curr"> ' + curr.currency + '</p>' +
+                '</div>' +
+                '<div class="currBlock__rate__nd">' +
+                    '<p class="currBlock__rate__nd-baseCurr">Sale Rate(ND): ' + curr.saleRateNB + '</p>' +
+                    '<p class="currBlock__rate__nd-curr">Sale Rate(ND): ' + curr.purchaseRateNB + '</p>' +
+                '</div>' +
+                '<div class="currBlock__rate">' +
+                    '<p class="currBlock__rate-baseCurr">Sale Rate: ' + curr.saleRate + '</p>' +
+                    '<p class="currBlock__rate-curr">Sale Rate: ' + curr.purchaseRate + '</p>' +           
+                '</div>' +
+            '</div>'
+            })
+            main.insertAdjacentHTML("afterbegin", currency.join(' '))
+        })
+}
+
+GET_CURR_API()
